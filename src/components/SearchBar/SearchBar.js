310103315a6283726d4731e87ecb17155b78e453
Dj2035 <dk.jude@yahoo.com>
2023-03-08 03:06:28 +0100

    Added the different methods for interacting with the SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,19 +2,76 @@ import React from "react";
 import "./SearchBar.css";
 
 //The search bar should allow users to businesses by the following keys
-const sortByOptions = {
-  "Best Match": "best_match",
-  "Highest Rated": "rating",
-  "Most Reviewed": "review_count"
-}
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match'
+    };
+
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+
+    this.sortByOptions = {
+      "Best Match": "best_match",
+      "Highest Rated": "rating",
+      "Most Reviewed": "review_count"
+    };
+  }
 
   renderSortByOptions() {
-    return Object.keys(sortByOptions).map(sortByOption => {
-      const sortByOptionValue = sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>;
+    return Object.keys(this.sortByOptions).map(sortByOption => {
+      const sortByOptionValue = this.sortByOptions[sortByOption];
+      return (
+        <li
+          key={sortByOptionValue}
+          className={this.getSortByClass(sortByOptionValue)}
+          onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+        >
+          {sortByOption}
+        </li>
+      );
     });
+  }
+
+  /* getSortByClass will conditionally style each sort by option, 
+    displaying to the user which sorting option is currently selected*/
+  getSortByClass(sortByOption) {
+    if (this.state.sortBy === sortByOption) {
+      return 'active';
+    }
+    return '';
+  }
+
+  handleSortByChange(sortByOption) {
+    this.setState({
+      sortBy: sortByOption
+    });
+  }
+
+  handleTermChange(e) {
+    this.setState({
+      term: e.target.value
+    });
+  }
+
+  handleLocationChange(e) {
+    this.setState({
+      location: e.target.value
+    });
+  }
+
+  handleSearch(e) {
+    const term = this.state.term;
+    const location = this.state.location;
+    const sortBy = this.state.sortBy;
+    this.props.searchYelp(term, location, sortBy);
+    e.preventDefault();
   }
 
   render() {
@@ -26,11 +83,11 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+          <input onChange={this.handleLocationChange} placeholder="Where?" />
         </div>
         <div className="SearchBar-submit">
-          <a href="$">Let's Go</a>
+          <button onClick={this.handleSearch}>Let's Go</button>
         </div>
       </div>
     );
